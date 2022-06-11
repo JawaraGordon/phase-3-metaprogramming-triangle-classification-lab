fcbe459f06eedf92b296e25954bb1ea116f9e6c8
JawaraGordon <jawaragordon@icloud.com>
2022-06-11 11:23:26 -0400

    commit tree c7dd4beba5b3362b203f84ab7631f54a9b8992df

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
-end
+    attr_reader :a, :b, :c
+
+    def initialize(a, b, c)
+      @a = a
+      @b = b
+      @c = c
+    end
+
+
+    def kind
+      if !self.a.positive? || !self.b.positive? || !self.c.positive? 
+          raise TriangleError
+      end
+
+      if a + b <= c || a + c <= b || b + c <= a 
+        raise TriangleError
+      end
+
+      if a == b && b == c
+        :equilateral
+       elsif a == b || b == c || a == c
+         :isosceles
+       else
+         :scalene
+        end
+       end
+       class TriangleError < StandardError
+       end
+      end
+  
+    
+   
